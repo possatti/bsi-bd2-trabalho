94bc06117e8e8b8d968378d850c749a90074a2be
Lucas Possatti <lucas_possatti@hotmail.com>
2014-05-18 17:10:23 -0300

    Criação do script sql para criação dos triggers.
    Foi criado um único trigger até então, que é o trigger para a view
    REGISTRO_CLIENTE. E a devida SP também for criada para ser acionada
    pela trigger.

---
+++ criar-triggers.sql
@@ -0,0 +1,76 @@
+-- A função será usada na trigger da view REGISTRO_CLIENTE. Ela
+-- intercepta todos os comandos exceto o SELECT para realizar as
+-- operações adequadamente em cada tabela que é usada na view.
+CREATE FUNCTION iud_registro_cliente()
+    RETURNS trigger AS $iud_registro_cliente$
+
+    DECLARE
+    id_endereco BIGINT;
+    id_cliente BIGINT;
+
+    BEGIN
+        -- Se a operação for de inserção, inserimos os dados nas tabelas adequadas.
+        IF (TG_OP = 'INSERT') THEN
+            -- Obtém o valor do próximo id que deverá ser usado.
+            id_endereco := nextval('endereco_id_seq');
+
+            -- Insere o endereço.
+            INSERT INTO ENDERECO(ID, CEP, ESTADO, CIDADE, BAIRRO, LOGRADOURO, NUMERO, PTO_REFERENCIA)
+            VALUES (id_endereco, NEW.CEP, NEW.ESTADO, NEW.CIDADE, NEW.BAIRRO,
+                NEW.LOGRADOURO, NEW.NUMERO, NEW.PTO_REFERENCIA);
+
+
+            -- Insere o cliente, usando o valor de id do endereço para referencia-lo.
+            INSERT INTO CLIENTE
+            VALUES (NEW.CNPJ, NEW.NOME, NEW.TELEFONE, NEW.ENDERECO_ID, id_endereco);
+
+            RETURN NEW;
+
+        -- Se a operação for um update, atualizamos os dados nas tabelas adequadas.
+        ELSIF (TG_OP = 'UPDATE') THEN
+            -- Busca pelo id do cliente e do seu endereço.
+            SELECT ID, ENDERECO_ID
+            INTO id_cliente, id_endereco
+            FROM CLIENTE
+            WHERE CPF = OLD.CPF;
+
+            -- Atualiza os dados do cliente.
+            UPDATE CLIENTE
+            SET (CNPJ, NOME, TELEFONE) =
+                (NEW.CNPJ, NEW.NOME, NEW.TELEFONE)
+            WHERE ID = id_cliente;
+
+            -- Atualiza os dados do endereço.
+            UPDATE ENDERECO
+            SET (CEP, ESTADO, CIDADE, BAIRRO, LOGRADOURO, NUMERO, PTO_REFERENCIA) =
+                (NEW.CEP, NEW.ESTADO, NEW.CIDADE, NEW.BAIRRO, NEW.LOGRADOURO, NEW.NUMERO, NEW.PTO_REFERENCIA)
+            WHERE ID = id_endereco;
+
+            RETURN NEW;
+
+        -- Se a operação for de deleção, apagamos os registros do respectivo cliente e endereço.
+        ELSIF (TG_OP = 'DELETE') THEN
+            -- Busca pelo id do cliente e do seu endereço.
+            SELECT ID, ENDERECO_ID
+            INTO id_cliente, id_endereco
+            FROM CLIENTE
+            WHERE CPF = OLD.CPF;
+
+            -- Apaga o registro do cliente.
+            DELETE FROM CLIENTE
+            WHERE OLD.ID = id_cliente;
+
+            -- Apaga o registro do endereço.
+            DELETE FROM ENDERECO
+            WHERE OLD.ENDERECO_ID = id_endereco;
+
+            RETURN OLD;
+        END IF;
+    END;
+$iud_registro_cliente$ LANGUAGE plpgsql;
+
+-- Trigger para a view REGISTRO_CLIENTE. Ela é usada para
+-- interceptar os comando de INSERT, UPDATE e DELETE.
+CREATE TRIGGER IUD_REGISTRO_CLIENTE
+    INSTEAD OF INSERT OR UPDATE OR DELETE ON REGISTRO_CLIENTE
+    FOR EACH ROW EXECUTE PROCEDURE iud_registro_cliente();
