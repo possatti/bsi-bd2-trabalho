3e1d92affcdf71c5afba0728ab11a5bcb239698f
Lucas Possatti <lucas_possatti@hotmail.com>
2014-05-25 01:10:24 -0300

    O script de criação de tabelas foi refeito.
    O script foi refeito de acordo com as novas mudanças no modelo.

--- criar-tabelas.sql
+++ criar-tabelas.sql
@@ -1,95 +1,156 @@
-﻿CREATE TABLE ENDERECO (
-    ID SERIAL PRIMARY KEY,
-    CEP CHAR(10) NOT NULL,
-    ESTADO CHAR(2) NOT NULL,
-    CIDADE VARCHAR(255) NOT NULL,
-    BAIRRO VARCHAR(255) NOT NULL,
-    LOGRADOURO VARCHAR(255) NOT NULL,
-    NUMERO VARCHAR(10),
-    PTO_REFERENCIA VARCHAR(255),
+﻿-- -----------------------------------------------------
+-- Table cliente
+-- -----------------------------------------------------
+CREATE TABLE cliente (
+    id SERIAL,
+    cnpj CHAR(18) NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    telefone1 VARCHAR(14) NULL,
+    telefone2 VARCHAR(14) NULL,
 
-    CONSTRAINT formato_cep CHECK(cep ~ '^\d{2}\.\d{3}-\d{3}$'),
-    CONSTRAINT formato_estado CHECK(estado ~ '^[A-Z]{2}$'),
-    CONSTRAINT formato_numero CHECK(numero ~ '^([1-9]\d{1,9})|(S/N)$')
+    PRIMARY KEY (id),
+    CONSTRAINT unique_cnpj UNIQUE(cnpj)
 );
 
-CREATE TABLE VEICULO (
-    ID SERIAL PRIMARY KEY,
-    PLACA CHAR(8) NOT NULL,
-    MARCA VARCHAR(45) NOT NULL,
-    MODELO VARCHAR(45) NOT NULL,
+-- -----------------------------------------------------
+-- Table endereco
+-- -----------------------------------------------------
+CREATE TABLE endereco (
+    id SERIAL,
+    cep CHAR(10) NOT NULL,
+    estado CHAR(2) NOT NULL,
+    cidade VARCHAR(255) NOT NULL,
+    bairro VARCHAR(255) NOT NULL,
+    logradouro VARCHAR(255) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    ponto_referencia TEXT NULL,
 
-    CONSTRAINT unique_placa UNIQUE(placa),
-    CONSTRAINT formato_placa CHECK(placa ~ '^[A-Z]{3}-\d{4}$')
+    PRIMARY KEY (id),
+    CONSTRAINT unique_endereco
+        UNIQUE(cep, estado, cidade, bairro, logradouro, numero)
 );
 
-CREATE TABLE MOTORISTA (
-    ID SERIAL PRIMARY KEY,
-    CPF CHAR(14) NOT NULL,
-    NOME VARCHAR(255) NOT NULL,
-    CNH VARCHAR(20) NOT NULL,
-    RG VARCHAR(45) NOT NULL,
-    TELEFONE VARCHAR(14) NOT NULL,
-    DISPONIVEL BOOLEAN NOT NULL DEFAULT false,
-    ENDERECO_ID INT NOT NULL REFERENCES endereco ON DELETE RESTRICT,
-    VEICULO_ID INT REFERENCES veiculo ON DELETE SET DEFAULT NULL,
+-- -----------------------------------------------------
+-- Table motorista
+-- -----------------------------------------------------
+CREATE TABLE motorista (
+    id SERIAL,
+    cpf CHAR(14) NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    cnh VARCHAR(20) NOT NULL,
+    rg VARCHAR(20) NOT NULL,
+    telefone1 VARCHAR(14) NULL,
+    telefone2 VARCHAR(14) NULL,
+    disponivel BOOLEAN NOT NULL,
 
+    PRIMARY KEY (id),
     CONSTRAINT unique_cpf UNIQUE(cpf),
-    CONSTRAINT unique_rg UNIQUE(rg),
-    CONSTRAINT unique_motorista_endereco_id UNIQUE(endereco_id),
-
-    CONSTRAINT formato_cpf CHECK(cpf ~ '^\d{3}\.\d{3}\.\d{3}-\d{2}$'),
-    CONSTRAINT formato_telefone CHECK(telefone ~ '^\(\d{2}\)(\d)?\d{4}-\d{4}$')
+    CONSTRAINT unique_cnh UNIQUE(cnh),
+    CONSTRAINT unique_rg UNIQUE(rg)
 );
 
-CREATE TABLE CLIENTE (
-    ID SERIAL PRIMARY KEY,
-    CNPJ CHAR(18) NOT NULL UNIQUE,
-    NOME VARCHAR(255) NOT NULL,
-    TELEFONE VARCHAR(14) NOT NULL,
-    ENDERECO_ID INT NOT NULL REFERENCES endereco ON DELETE RESTRICT,
+-- -----------------------------------------------------
+-- Table veiculo
+-- -----------------------------------------------------
+CREATE TABLE veiculo (
+    id SERIAL,
+    placa CHAR(8) NOT NULL,
+    marca VARCHAR(45) NOT NULL,
+    modelo VARCHAR(45) NOT NULL,
 
-    CONSTRAINT unique_cnpj UNIQUE(cnpj),
-    CONSTRAINT unique_cliente_endereco_id UNIQUE(endereco_id),
-    
-    CONSTRAINT formato_cnpj CHECK(cnpj ~ '^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$'),
-    CONSTRAINT formato_telefone CHECK(telefone ~ '^\(\d{2}\)(\d)?\d{4}-\d{4}$')
+    PRIMARY KEY (id),
+    CONSTRAINT unique_placa UNIQUE(placa)
 );
 
-CREATE TABLE PEDIDO (
-    ID SERIAL PRIMARY KEY,
-    TIMESTAMP_REQUISICAO TIMESTAMP NOT NULL,
-    PRODUTO VARCHAR(255) NOT NULL,
-    QTD_VOLUMES INT NOT NULL DEFAULT 0,
-    PESO_ENCOMENDA INT NOT NULL DEFAULT 0,
-    DISTANCIA INT,
-    PRECO_FRETE DECIMAL(10,2),
-    OBSERVACOES VARCHAR(255),
-    STATUS VARCHAR(20) NOT NULL DEFAULT 'EM_PROCESSAMENTO',
-    CLIENTE_ID INT NOT NULL REFERENCES cliente ON DELETE CASCADE,
-    ENDERECO_ORIGEM INT NOT NULL REFERENCES endereco ON DELETE RESTRICT,
-    ENDERECO_DESTINO INT NOT NULL REFERENCES endereco ON DELETE RESTRICT,
+-- -----------------------------------------------------
+-- Table pedido
+-- -----------------------------------------------------
+CREATE TABLE pedido (
+    id SERIAL,
+    produto VARCHAR(255) NOT NULL,
+    momento_pedido TIMESTAMP NOT NULL,
+    situacao VARCHAR(45) NOT NULL,
+    qtd_volumes INT NULL,
+    peso_liquido DECIMAL(10,3) NULL,
+    preco_frete DECIMAL(10,2) NULL,
+    observacoes TEXT NULL,
+    cliente_id INT NOT NULL,
+    endereco_origem_id INT NOT NULL,
+    endereco_destino_id INT NOT NULL,
 
-    CONSTRAINT uniques_cliente UNIQUE(timestamp_requisicao, cliente_id),
-
-    CONSTRAINT enum_status
-    CHECK (status LIKE 'EM_PROCESSAMENTO'
-        OR status LIKE 'EM_SEPARACAO'
-        OR status LIKE 'ENVIADO'
-        OR status LIKE 'RECEBIDO'
-        )
+    PRIMARY KEY (id),
+    CONSTRAINT unique_pedido UNIQUE(momento_pedido, cliente_id),
+    CONSTRAINT fk_pedido_cliente
+        FOREIGN KEY (cliente_id)
+        REFERENCES cliente (id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_pedido_endereco_origem
+        FOREIGN KEY (endereco_origem_id)
+        REFERENCES endereco (id)
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_pedido_endereco_destino
+        FOREIGN KEY (endereco_destino_id)
+        REFERENCES endereco (id)
+        ON DELETE RESTRICT
 );
 
-CREATE TABLE VIAGEM (
-    ID SERIAL PRIMARY KEY,
-    QTD_VOLUMES INT NOT NULL,
-    PESO_VOLUMES INT NOT NULL,
-    TIMESTAMP_INICIO TIMESTAMP NOT NULL,
-    TIMESTAMP_FIM TIMESTAMP,
-    OBSERVACOES VARCHAR(255),
-    MOTORISTA_ID INT REFERENCES motorista ON DELETE RESTRICT,
-    PEDIDO_ID INT REFERENCES pedido ON DELETE CASCADE,
+-- -----------------------------------------------------
+-- Table viagem
+-- -----------------------------------------------------
+CREATE TABLE viagem (
+    id SERIAL,
+    momento_saida TIMESTAMP NULL,
+    momento_chegada TIMESTAMP NULL,
+    veiculo_id INT NOT NULL,
 
-    CONSTRAINT uniques_viagem UNIQUE(timestamp_inicio, motorista_id),
-    CONSTRAINT datas_inicio_fim CHECK((TIMESTAMP_INICIO < TIMESTAMP_FIM) OR (TIMESTAMP_FIM IS NULL))
+    PRIMARY KEY (id),
+    CONSTRAINT unique_viagem UNIQUE(momento_saida, veiculo_id),
+    CONSTRAINT fk_viagem_veiculo
+        FOREIGN KEY (veiculo_id)
+        REFERENCES veiculo (id)
+        ON DELETE RESTRICT
+);
+
+-- -----------------------------------------------------
+-- Table carga
+-- -----------------------------------------------------
+CREATE TABLE carga (
+    pedido_id BIGINT NOT NULL,
+    viagem_id BIGINT NOT NULL,
+    qtd_volumes INT NULL,
+    momento_carregamento TIMESTAMP NULL,
+    momento_descarregamento TIMESTAMP NULL,
+
+    PRIMARY KEY (pedido_id, viagem_id),
+    CONSTRAINT fk_carga_pedido
+        FOREIGN KEY (pedido_id)
+        REFERENCES pedido (id)
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_carga_viagem
+        FOREIGN KEY (viagem_id)
+        REFERENCES viagem (id)
+        ON DELETE RESTRICT
+);
+
+-- -----------------------------------------------------
+-- Table motorista_veiculo
+-- -----------------------------------------------------
+CREATE TABLE motorista_veiculo (
+    id SERIAL,
+    veiculo_id INT NOT NULL,
+    motorista_id INT NOT NULL,
+    momento_alocacao TIMESTAMP NULL,
+    momento_liberacao VARCHAR(45) NULL,
+
+    PRIMARY KEY (id),
+    CONSTRAINT unique_motorista_veiculo
+        UNIQUE(veiculo_id, motorista_id, momento_alocacao),
+    CONSTRAINT fk_veiculo_has_motorista_veiculo
+        FOREIGN KEY (veiculo_id)
+        REFERENCES veiculo (id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_motorista_has_motorista_veiculo
+        FOREIGN KEY (motorista_id)
+        REFERENCES motorista (id)
+        ON DELETE CASCADE
 );
