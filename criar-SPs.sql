a5a41ba4f8472478229153d9acb26beb857207df
Lucas Possatti <lucas_possatti@hotmail.com>
2014-05-26 14:09:40 -0300

    Foi refeito o script de criação das SPs.
    As SPs foram refeitas segundo o novo modelo de dados, feito após as
    correções da professora.

--- criar-SPs.sql
+++ criar-SPs.sql
@@ -1,82 +1,129 @@
---
--- Filtra a tabela de pedidos, selecionando apenas os pedidos pertencentes
--- a um determinado cliente.
+-- ----------------------------------------------------------
+-- Esta SP retorna uma tabela com todos os pedidos que uma
+-- determinada viagem contribuiu ou está contribuindo para.
 -- 
 -- Argumentos:
---      cnpj_cliente: cnpj do cliente.
---
-CREATE FUNCTION pedidos_por_cliente(cnpj_cliente CHAR(18))
+--      id_viagem: id da viagem.
+-- ----------------------------------------------------------
+CREATE FUNCTION pedidos_da_viagem(id_viagem INT)
 RETURNS TABLE(
-    DATA_REQUISICAO TIMESTAMP,
-    PRODUTO VARCHAR(255), QTD_VOLUMES INT, PESO_ENCOMENDA INT,
-    DISTANCIA INT, PRECO_FRETE DECIMAL(10,2), OBSERVACOES VARCHAR(255),
-    STATUS VARCHAR(20))
+    id INT,
+    produto D_PRODUTO,
+    momento_pedido TIMESTAMP,
+    situacao STATUS_PEDIDO,
+    qtd_volumes INT,
+    peso_liquido D_PESO,
+    preco_frete D_PRECO,
+    observacoes TEXT,
+    cliente_id INT,
+    endereco_origem_id INT,
+    endereco_destino_id INT
+    )
 AS $$
     BEGIN
         RETURN QUERY
-            SELECT
-                P.TIMESTAMP_REQUISICAO, P.PRODUTO, P.QTD_VOLUMES,
-                P.PESO_ENCOMENDA, P.DISTANCIA, P.PRECO_FRETE,
-                P.OBSERVACOES, P.STATUS
-            FROM PEDIDO P
-            INNER JOIN CLIENTE C ON P.CLIENTE_ID = C.ID
-            WHERE C.CNPJ = cnpj_cliente;
+            SELECT *
+            FROM pedido p
+            WHERE p.id = ANY (
+                SELECT c.pedido_id
+                FROM carga c
+                WHERE viagem_id = id_viagem
+            );
     END;
 $$ LANGUAGE plpgsql;
 
---
--- Elabora uma tabela com um relatório mensal de pedidos. O relatório é um
--- resumo de cada pedido feito em um determinado mês.
+-- ----------------------------------------------------------
+-- Seleciona todos os pedidos de um determinado cliente
+-- ordenando pelos mais recentes.
+-- 
+-- Argumentos:
+--      id_viagem: id da viagem.
+-- ----------------------------------------------------------
+CREATE FUNCTION pedidos_do_cliente(id_cliente INT)
+RETURNS TABLE(
+    id INT,
+    produto D_PRODUTO,
+    momento_pedido TIMESTAMP,
+    situacao STATUS_PEDIDO,
+    qtd_volumes INT,
+    peso_liquido D_PESO,
+    preco_frete D_PRECO,
+    observacoes TEXT,
+    cliente_id INT,
+    endereco_origem_id INT,
+    endereco_destino_id INT
+    )
+AS $$
+    BEGIN
+        RETURN QUERY
+            SELECT *
+            FROM pedido p
+            WHERE p.cliente_id = id_cliente
+            ORDER BY momento_pedido;
+    END;
+$$ LANGUAGE plpgsql;
+
+-- ----------------------------------------------------------
+-- Elabora uma tabela com um relatório mensal de pedidos. O
+-- relatório é um resumo de cada pedido feito em um
+-- determinado mês.
 --
 -- Argumentos:
 --      mes: mês dos pedidos a serem selecionados.
 --      ano: ano dos pedidos a serem selecionados.
---
+-- ----------------------------------------------------------
 CREATE FUNCTION relatorio_mensal_pedidos(mes INT, ano INT)
 RETURNS TABLE(
-    NOME_CLIENTE VARCHAR(255),
-    PRODUTO VARCHAR(255),
-    QTD_VOLUMES INT,
-    PESO_ENCOMENDA INT,
-    PRECO_FRETE DECIMAL(10,2),
-    STATUS VARCHAR(20),
-    DATA_REQUISICAO TIMESTAMP)
+    nome_cliente D_NOME,
+    produto D_PRODUTO,
+    qtd_volumes INT,
+    peso_encomenda D_PESO,
+    preco_frete D_PRECO,
+    status STATUS_PEDIDO,
+    data_requisicao TIMESTAMP)
 AS $$
     BEGIN
         RETURN QUERY
             SELECT
-                C.NOME, P.PRODUTO, P.QTD_VOLUMES, P.PESO_ENCOMENDA,
-                P.PRECO_FRETE, P.STATUS, P.TIMESTAMP_REQUISICAO
-            FROM PEDIDO P
-            INNER JOIN CLIENTE C ON P.CLIENTE_ID = C.ID
+                c.nome, p.produto, p.qtd_volumes, p.peso_encomenda,
+                p.preco_frete, p.status, p.timestamp_requisicao
+            FROM pedido p
+            INNER JOIN cliente c ON p.cliente_id = c.id
             WHERE
-                EXTRACT(YEAR FROM P.TIMESTAMP_REQUISICAO) = ano
-                AND EXTRACT(MONTH FROM P.TIMESTAMP_REQUISICAO) = mes;
+                EXTRACT(YEAR FROM p.timestamp_requisicao) = ano
+                AND EXTRACT(MONTH FROM p.timestamp_requisicao) = mes;
     END;
 $$ LANGUAGE plpgsql;
 
---
--- Calcula o tempo de duração médio das viagens por mês. Isto é, calcula
--- o tempo gasto pelos veículos, em média, para alcançar o destino. Para
--- isso a SP seleciona apenas as viagens que terminaram no mês indicado.
---
+-- ----------------------------------------------------------
+-- Calcula o tempo de duração médio das viagens por mês. Isto
+-- é, calcula o tempo médio gasto em que os veículos estão na
+-- estrada.
+-- 
+-- Por ser mensal, é necessário um critério para a seleção.
+-- Para isso a SP seleciona apenas as viagens que terminaram
+-- no mês indicado.
+-- 
 -- Argumentos:
 --      mes: mês em que os veículos terminaram a entrega.
 --      ano: ano em que os veículos terminaram a entrega.
---
+-- ----------------------------------------------------------
 CREATE FUNCTION duracao_media_mensal_viagens(mes INT, ano INT)
 RETURNS INTERVAL
 AS $$
     DECLARE
         media_tempo INTERVAL;
     BEGIN
+        -- Captura a média para a variável media_tempo.
         SELECT
-            avg(age(V.TIMESTAMP_INICIO, V.TIMESTAMP_FIM))
+            avg(age(v.timestamp_inicio, v.timestamp_fim))
         INTO media_tempo
-        FROM VIAGEM V
+        FROM viagem v
         WHERE
-            EXTRACT(YEAR FROM V.TIMESTAMP_FIM) = ano
-            AND EXTRACT(MONTH FROM V.TIMESTAMP_FIM) = mes;
+            EXTRACT(YEAR FROM v.momento_saida) = ano
+            AND EXTRACT(MONTH FROM v.momento_chegada) = mes;
+
+        -- Retorna a média
         RETURN media_tempo;
     END;
 $$ LANGUAGE plpgsql;
